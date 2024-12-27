8eb2702f43ce9b6fb681860cfb3479773e3618bf
Pramudya <pramudyap333@gmail.com>
2024-12-27 23:16:16 +0700

    adding theme

---
+++ lib/theme.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+double defaultMargin = 21;
+
+Color primaryColor = Color(0xffA66DD7);
+Color secondaryColor = Color(0xffD76DB8);
+Color alertColor = Color(0xffED6363);
+Color priceColor = Color(0xff2C96F1);
+Color primaryTextColor = Color(0xff000000);
+Color secondaryTextColor = Color(0xff999999);
+Color shadowColor = Color(0xffB7A1D3);
+Color c4Color = Color(0xffC4C4C4);
+Color whiteBone = Color(0xffF1F1F1);
+
+TextStyle titleTextStyle = GoogleFonts.pacifico(
+  color: primaryTextColor,
+);
+
+TextStyle primaryTextStyle = GoogleFonts.nunito(
+  color: primaryTextColor,
+);
+
+TextStyle secondaryTextStyle = GoogleFonts.pacifico(
+  color: secondaryTextColor,
+);
