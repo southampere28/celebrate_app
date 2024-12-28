219091560b257f8cf8d049cdd739d4bac9c4d52e
Pramudya <pramudyap333@gmail.com>
2024-12-28 20:25:52 +0700

    update theme

--- lib/theme.dart
+++ lib/theme.dart
@@ -21,6 +21,16 @@ TextStyle primaryTextStyle = GoogleFonts.nunito(
   color: primaryTextColor,
 );
 
+TextStyle buttonTextStyle = GoogleFonts.poppins(
+  color: whiteBone
+);
+
 TextStyle secondaryTextStyle = GoogleFonts.pacifico(
   color: secondaryTextColor,
 );
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
