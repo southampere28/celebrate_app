3b5c2e6563fbc5e3dcaddbf4d6ff0ac5d38b9a99
Pramudya <pramudyap333@gmail.com>
2025-01-27 23:33:26 +0700

    update content provider and implement new json data

--- lib/provider/content_provider.dart
+++ lib/provider/content_provider.dart
@@ -3,44 +3,32 @@ import 'dart:convert';
 import 'package:flutter/services.dart' show rootBundle;
 
 class ContentProvider with ChangeNotifier {
-  final List<String> _images = [
-    'assets/sample.jpg',
-    'assets/sample2.jpg',
-    'assets/sample.jpg'
-  ];
-  final List<String> _titles = ['title1', 'title2', 'title3'];
-  final List<String> _greetings = ['greeting1', 'greeting2', 'greeting3'];
-  final String _senderName = "Pramudya Putra";
-
-  String _titleContent = '';
-  String _greetingContent = '';
+  List<Map<String, dynamic>> _contentList = [];
   int _currentIndex = 0;
-  Map<String, dynamic>? _jsonData;
+  String _senderName = "Pramudya Putra";
 
   // Getters
-  List<String> get images => _images;
-  String get titleContent => _titleContent;
-  String get greetingContent => _greetingContent;
+  List<Map<String, dynamic>> get contentList => _contentList;
+  String get currentTitle => _contentList.isNotEmpty ? _contentList[_currentIndex]['title'] : '';
+  String get currentGreeting => _contentList.isNotEmpty ? _contentList[_currentIndex]['greeting'] : '';
+  String get currentImage => _contentList.isNotEmpty ? _contentList[_currentIndex]['image'] : '';
   int get currentIndex => _currentIndex;
   String get senderName => _senderName;
 
   // Method to load JSON data
   Future<void> loadJsonData() async {
     final String response = await rootBundle.loadString('assets/data.json');
-    final data = json.decode(response);
-    _jsonData = data;
-
-    // Initialize the first content
-    _titleContent = _jsonData?[_titles[0]] ?? '';
-    _greetingContent = _jsonData?[_greetings[0]] ?? '';
+    final List<dynamic> data = json.decode(response);
+    _contentList = data.map((e) => Map<String, dynamic>.from(e)).toList();
+    _currentIndex = 0; // Reset to the first content
     notifyListeners();
   }
 
   // Method to update content based on the index
   void updateContent(int index) {
-    _currentIndex = index;
-    _titleContent = _jsonData?[_titles[index]] ?? '';
-    _greetingContent = _jsonData?[_greetings[index]] ?? '';
-    notifyListeners();
+    if (index >= 0 && index < _contentList.length) {
+      _currentIndex = index;
+      notifyListeners();
+    }
   }
 }
