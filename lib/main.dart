5e59a5fd9e3b73f7ebb2e5ce8e92fdb95dee8963
Pramudya <pramudyap333@gmail.com>
2025-02-06 22:48:54 +0700

    update named route

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'dart:developer';
 
 import 'package:celebrate_app/content_page.dart';
+import 'package:celebrate_app/create_page.dart';
 import 'package:celebrate_app/feedback_page.dart';
 import 'package:celebrate_app/provider/content_provider.dart';
 import 'package:celebrate_app/provider/response_provider.dart';
+import 'package:celebrate_app/share_page.dart';
 import 'package:celebrate_app/splash_page.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -24,18 +26,24 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(
-            create: (context) => ContentProvider()..loadJsonData(),),
-        ChangeNotifierProvider(
-            create: (context) => ResponseProvider()),
+          create: (context) => ContentProvider()..loadJsonData(),
+        ),
+        ChangeNotifierProvider(create: (context) => ResponseProvider()),
       ],
       child: MaterialApp(
           title: 'Celebrate App',
-          debugShowCheckedModeBanner: true,
+          debugShowCheckedModeBanner: false,
+          routes: {
+            '/': (context) => const SplashPage(),
+            '/content': (context) => const ContentPage(),
+            '/feedback-page': (context) => const FeedBackPage(),
+            '/create-page': (context) => const CreatePage(),
+          },
           theme: ThemeData(
             colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
             useMaterial3: true,
           ),
-          home: const FeedBackPage()),
+          home: const ContentPage()),
     );
   }
 }
